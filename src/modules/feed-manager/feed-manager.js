a9e7e995f3c17a32952bd37062f994de144960e7
vitalii.mishchenko <vitalii.mishchenko@globallogic.com>
2016-11-07 19:06:38 +0200

    feed manager refactoring

---
+++ src/modules/feed-manager/feed-manager.js
@@ -0,0 +1,78 @@
+'use strict';
+
+class FeedManager {
+    constructor(feedParserFactory, FeedResource, log) {
+        this.feedParserFactory = feedParserFactory;
+        this.FeedResource = FeedResource;
+        this.log = log;
+    }
+
+    getFeeds() {
+        return this.FeedResource.find({});
+    }
+
+    canTrackFeed(feedUrl) {
+        let me = this;
+
+        if (feedUrl) {
+            return new Promise(function (resolve, reject) {
+                let feedParser = me.feedParserFactory.get();
+                feedParser.feedUrl = feedUrl;
+
+                let feed = me.FeedResource.findOne({
+                    url: feedUrl
+                });
+
+                Promise.all([
+                    feed,
+                    feedParser.parse()
+                ]).then(function (data) {
+                    let feed = data[0];
+                    let parsedFeed = data[1];
+
+                    if (feed) {
+                        reject({
+                            message: 'Feed already exist'
+                        });
+                    } else {
+                        if (!parsedFeed) {
+                            reject({
+                                message: 'Cannot parse feed'
+                            });
+                        } else {
+                            resolve(parsedFeed);
+                        }
+                    }
+                }).catch(function (error) {
+                    reject({
+                        message: 'Cannot track due to: ' + error.message
+                    })
+                });
+            });
+        } else {
+            return Promise.reject({
+                message: 'Invalid feed url'
+            });
+        }
+    }
+
+    trackFeed(feedUrl) {
+        let me = this;
+
+        return this.canTrackFeed(feedUrl).then(function (feed) {
+            return me.FeedResource.create(feed).then(function (feedResource) {
+                return {
+                    _id: feedResource._id
+                }
+            }).catch(function (error) {
+                log.error(error);
+
+                return Promise.reject({
+                    message: 'Cannot create feed'
+                });
+            });
+        });
+    }
+}
+
+module.exports = FeedManager;
--- src/modules/feed-manager/index.js
+++ src/modules/feed-manager/index.js
@@ -1,79 +1,13 @@
 'use strict';
 
 let FeedResource = require('../resources').FeedResource;
-let FeedParser = require('../feed-parser');
+let feedParserFactory = require('../feed-parser');
 let log = require('mue-core/modules/log')(module);
 
-/**
- * Return all feeds
- * */
-function getFeeds() {
-    return FeedResource.find({});
-}
+let FeedManager = require('./feed-manager');
 
-function trackFeed(feedUrl) {
-    return canTrackFeed(feedUrl).then(function (feed) {
-        return FeedResource.create(feed).then(function (feedResource) {
-            return {
-                _id: feedResource._id
-            }
-        }).catch(function (error) {
-            log.error(error);
+let feedManager = new FeedManager(feedParserFactory, FeedResource, log);
 
-            return Promise.reject({
-                message: 'Cannot create feed'
-            });
-        });
-    });
-}
-
-function canTrackFeed(feedUrl) {
-    if (feedUrl) {
-        return new Promise(function (resolve, reject) {
-            let feedParser = new FeedParser.get();
-            feedParser.feedUrl = feedUrl;
-
-            let feed = FeedResource.findOne({
-                url: feedUrl
-            });
-
-            Promise.all([
-                feed,
-                feedParser.parse()
-            ]).then(function (data) {
-                let feed = data[0];
-                let parsedFeed = data[1];
-
-                if (feed) {
-                    reject({
-                        message: 'Feed already exist'
-                    });
-                } else {
-                    if (!parsedFeed) {
-                        reject({
-                            message: 'Cannot parse feed'
-                        });
-                    } else {
-                        resolve(parsedFeed);
-                    }
-                }
-            }).catch(function (error) {
-                reject({
-                    message: 'Cannot track due to: ' + error.message
-                })
-            });
-        });
-    } else {
-        return Promise.reject({
-            message: 'Invalid feed url'
-        });
-    }
-}
-
-module.exports = {
-    getFeeds: getFeeds,
-
-    trackFeed: trackFeed,
-
-    canTrackFeed: canTrackFeed
+exports.get = function () {
+    return feedManager;
 };
